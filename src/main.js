30139d6c575911ac1966b9b0a261f166679f2060
Taylor Somers <tsomers@yandex.com>
2020-07-03 11:18:14 -0400

    Add basic UI logic to receive API call data

--- src/main.js
+++ src/main.js
@@ -2,7 +2,26 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { ExchangeService } from './../src/exchange-service.js';
 
 $(document).ready(function() {
-  
+  $("#convert").click(function(e) {
+    (e).preventDefault();
+    let dollars = $("#dollars").val();
+    const exchangeArray = [];
+
+    (async () => {
+      let exchangeService = new ExchangeService();
+      const response = await exchangeService.getExchangeRates();
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
+        let exchangeValues = response.conversion_rates.AED;
+      } else {
+        $("#results").html(`There was an error handling your request.`);
+      }
+    }
+  });
 });
