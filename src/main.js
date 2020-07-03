eef60d07a030d927db3394bdb3da856d6f8a4432
Taylor Somers <tsomers@yandex.com>
2020-07-03 12:07:46 -0400

    Add logic to convert user-inputted dollar amount to foreign currency amounts

--- src/main.js
+++ src/main.js
@@ -4,11 +4,28 @@ import './styles.css';
 import $ from "jquery";
 import { ExchangeService } from './../src/exchange-service.js';
 
+// function convertDollars(input, conversionRates) {
+//   let convertedDollars = [];
+//   conversionRates.map(function() {
+//     convertedDollars.push(input * );
+//   });
+//   return convertedDollars;
+// }
+
+function convertDollars(input, conversionRates) {
+  let convertedDollars = [];
+  for (let i = 0; i < 5; i ++) {
+    convertedDollars.push((input * conversionRates[i]).toFixed(2));
+  }
+  return convertedDollars;
+}
+
 $(document).ready(function() {
   $("#convert").click(function(e) {
     (e).preventDefault();
     let dollars = $("#dollars").val();
     const exchangeArray = [];
+    let exchangeAmounts;
 
     (async () => {
       let exchangeService = new ExchangeService();
@@ -18,10 +35,16 @@ $(document).ready(function() {
 
     function getElements(response) {
       if (response) {
-        let exchangeValues = response.conversion_rates.AED;
+        let exchangeValues = [response.conversion_rates.BGN, response.conversion_rates.HUF, response.conversion_rates.KZT, response.conversion_rates.PLN, response.conversion_rates.RUB];
+        exchangeValues.map((element) => {
+          exchangeArray.push(element);
+        });
+        exchangeAmounts = convertDollars(dollars, exchangeArray);
+        $("#results").text(exchangeAmounts);
       } else {
         $("#results").html(`There was an error handling your request.`);
       }
     }
+
   });
 });
