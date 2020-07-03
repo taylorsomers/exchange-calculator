d5e86ac24e63af08d6ce1ca6448ba3adbc385650
Taylor Somers <tsomers@yandex.com>
2020-07-03 11:17:48 -0400

    Add async API call

---
+++ src/exchange-service.js
@@ -0,0 +1,21 @@
+export class ExchangeService {
+
+  constructor() {
+
+  }
+
+  async getExchangeRates() {
+    try {
+      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else{
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+}
